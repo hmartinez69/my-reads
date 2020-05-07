b38db7bd2ee1f7d1801b5b52d66b52b9aa4005f2
hugomartinez <hugo.martinez@supdepub.com>
2020-05-07 12:56:13 +0200

    Creating Components

--- src/App.js
+++ src/App.js
@@ -2,38 +2,182 @@ import React, { Component } from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route, Link } from 'react-router-dom';
+import getAll from './data';
 
-class BooksApp extends React.Component {
-    state = {}
+class BooksApp extends Component {
+    bookshelves = [
+        { key: 'currentlyReading', name: 'Currently Reading' },
+        { key: 'wantToRead', name: 'Want to Read' },
+        { key: 'read', name: 'Have Read' },
+    ];
+
+    state = {
+        books: getAll,
+    }
 
     render() {
+        const { books } = this.state;
         return (
             <div className="app">
-                <Route exact path="/" component={BookList} />
-                <Route path="/search" component={BookSearch} />
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
+                        <ListBooks bookshelves={this.bookshelves} books={books} />
+                    )}
+                />
+                <Route path="/search" render={() => <SearchBooks books={books} />} />
             </div>
-        )
+        );
     }
 }
 
-class BookList extends Component {
+class ListBooks extends Component {
     render() {
+        const { bookshelves, books } = this.props;
         return (
             <div className="list-books">
-                {/* UI code */}
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <Bookcase bookshelves={bookshelves} books={books} />
+                <OpenSearchButton />
             </div>
-        )
+        );
     }
 }
 
-class BookSearch extends Component {
+const OpenSearchButton = () => {
+    return (
+        <div className="open-search">
+            <Link to="search">
+                <button>Add a Book</button>
+            </Link>
+        </div>
+    );
+};
+
+const Bookcase = props => {
+    const { bookshelves, books } = props;
+    return (
+        <div className="list-books-content">
+            <div>
+                {bookshelves.map(shelf => (
+                    <Bookshelf key={shelf.key} shelf={shelf} books={books} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+const Bookshelf = props => {
+    const { shelf, books } = props;
+    const booksOnThisShelf = books.filter(book => book.shelf === shelf.key);
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf.name}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {booksOnThisShelf.map(book => (
+                        <Book key={book.id} book={book} shelf={shelf.key} />
+                    ))}
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+const Book = props => {
+    const { book, shelf } = props;
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                        }}
+                    />
+                    <BookshelfChanger book={book} shelf={shelf} />
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors.join(', ')}</div>
+            </div>
+        </li>
+    );
+};
+
+class BookshelfChanger extends Component {
     render() {
         return (
-            <div className="search-books">
-                {/* UI code */}
+            <div className="book-shelf-changer">
+                <select value={this.props.shelf}>
+                    <option value="move" disabled>
+                        Move to...
+                    </option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
             </div>
-        )
+        );
     }
 }
+
+class SearchBooks extends Component {
+    render() {
+        const { books } = this.props;
+        return (
+            <div className="search-books">
+                <SearchBar />
+                <SearchResults books={books} />
+            </div>
+        );
+    }
+}
+
+const SearchBar = props => {
+    return (
+        <div className="search-books-bar">
+            <CloseSearchButton />
+            <SearchBooksInput />
+        </div>
+    );
+};
+
+const CloseSearchButton = () => {
+    return (
+        <Link to="/">
+            <button className="close-search">Close</button>
+        </Link>
+    );
+};
+
+class SearchBooksInput extends Component {
+    render() {
+        return (
+            <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author" />
+            </div>
+        );
+    }
+}
+
+const SearchResults = props => {
+    const { books } = props;
+    return (
+        <div className="search-books-results">
+            <ol className="books-grid">
+                {books.map(book => (
+                    <Book key={book.id} book={book} shelf="none" />
+                ))}
+            </ol>
+        </div>
+    );
+};
 
 export default BooksApp
